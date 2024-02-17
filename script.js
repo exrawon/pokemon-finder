1e499da118c47200689e0bfc423323df7bd5697f
exrawon <152187690+exrawon@users.noreply.github.com>
2024-02-18 03:30:00 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,40 +1,33 @@
+const userInput = document.getElementById('pokemon-name');
 const searchButton = document.getElementById('search-button');
 const output = document.getElementById('output');
 const errorMessage = document.getElementById('error-message');
-const nameDisplay = document.getElementById('name-display');
+
 const getPokemon = async () => {
 	try {
-		const pokemonName = document
-			.getElementById('pokemon-name')
-			.value.toLowerCase();
+		const pokemonName = userInput.value.toLowerCase();
+		errorMessage.classList.add('hidden');
+
 		if (pokemonName) {
+			output.innerHTML = `<img src="./assets/loading.gif" id="sprite" />`;
 			const response = await fetch(
 				`https://pokeapi.co/api/v2/pokemon/${pokemonName}`
 			);
-			while (output.firstChild) {
-				output.removeChild(output.firstChild);
-			}
+
 			if (!response.ok) {
 				console.log(response.status);
-				const errorImage = new Image();
-				errorImage.src = `./assets/404.png`;
-				output.appendChild(errorImage);
-				output.innerHTML += `<span id="name-display">NOT FOUND</span>`;
+				output.innerHTML = `<img src="./assets/404.png" id="sprite" /><span id="name-display">NOT FOUND</span>`;
 				errorMessage.classList.remove('hidden');
+				document.getElementById('pokemon-name').value = '';
 				return;
 			}
 
 			const data = await response.json();
-			errorMessage.classList.add('hidden');
 
-			const pokemonImage = new Image();
-			pokemonImage.alt = 'Image of Ship';
-			pokemonImage.src = `${data.sprites.front_default}`;
-			output.appendChild(pokemonImage);
-			console.log(data);
-			output.innerHTML += `<span id="name-display">#${
-				data.id
-			} ${data.name.toUpperCase()}</span>`;
+			output.innerHTML = `<img src="${
+				data.sprites.front_default
+			}" id="sprite" />
+			<span id="name-display">#${data.id} ${data.name.toUpperCase()}</span>`;
 			document.getElementById('pokemon-name').value = '';
 		}
 	} catch (error) {
@@ -42,16 +35,9 @@ const getPokemon = async () => {
 	}
 };
 
-const runApp = () => {
-	getPokemon();
-	searchButton.disabled = true;
-	setTimeout(() => {
-		searchButton.disabled = false;
-	}, 3000);
-};
-searchButton.addEventListener('click', runApp);
+searchButton.addEventListener('click', getPokemon);
 document.addEventListener('keypress', (e) => {
 	if (e.key === 'Enter') {
-		runApp();
+		getPokemon();
 	}
 });
